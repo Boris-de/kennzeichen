fb970383b07db901907c78d4551ad2095790ecc9
Boris Wachtmeister <boris-code@gmx.com>
2016-05-07 12:30:03 +0200

    use https where possible

--- src/main/java/de/berlios/kennzeichen/AboutBox.java
+++ src/main/java/de/berlios/kennzeichen/AboutBox.java
@@ -47,7 +47,7 @@ public class AboutBox extends Form implements CommandListener {
     public AboutBox(Display disp) {
         super("Hilfe"); //call inherited constructor
         append(new StringItem(null, "Zur Suche einfach das Kennzeichen in die Box eingeben und mit OK best\u00E4tigen.\n"));
-        append(new StringItem(null, "\nMehr Informationen unter\nhttp://kennzeichen.code.achterblog.de"));
+        append(new StringItem(null, "\nMehr Informationen unter\nhttps://kennzeichen.code.achterblog.de"));
         append(new StringItem(null, "\nVielen Dank an folgende Helfer:\nUdo Roscher"));
         addCommand(new Command("OK", Command.OK, 0));
         setCommandListener(this);
